023f1bca1b10d2766e8647a035605f35fa8dcd43
Anna Kim <anna.kim93@gmail.com>
2021-01-14 00:06:30 -0500

    set up new card form comp

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ const App = () => {
   return (
     <section>
       <header className="header">
-        <h1 className="header__h1"><span className="header__text">Inspiration Board</span></h1>
+        <h1 className="header__h1">
+          <span className="header__text">Inspiration Board</span>
+        </h1>
       </header>
       <Board
         url="https://inspiration-board.herokuapp.com/"
--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -3,4 +3,18 @@ import PropTypes from 'prop-types';
 import emoji from 'emoji-dictionary';
 import './NewCardForm.css';
 
-const EMOJI_LIST = ["", "heart_eyes", "beer", "clap", "sparkling_heart", "heart_eyes_cat", "dog"]
+const EMOJI_LIST = [
+  "", 
+  "heart_eyes", 
+  "beer", 
+  "clap", 
+  "sparkling_heart", 
+  "heart_eyes_cat", 
+  "dog"
+]
+
+const NewCardForm = (props) => {
+
+};
+
+export default NewCardForm;
