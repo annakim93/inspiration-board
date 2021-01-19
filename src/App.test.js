7dd872d8e64367739c510431f0ea85a65bfa49a9
Anna Kim <anna.kim93@gmail.com>
2021-01-18 21:02:29 -0500

    media query for phone screen - portrait

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import '@testing-library/jest-dom'
 import { render, screen} from '@testing-library/react'
 
 import App from './App';
 
 describe('App', () => {
-
   it('renders without crashing', () => {
-    render(<App />);
-    expect(screen.getByText(/Inspo Board/i)).toBeInTheDocument();
+    const section = document.createElement('section');
+    ReactDOM.render(<App />, section);
+    ReactDOM.unmountComponentAtNode(section);
   });
 });
